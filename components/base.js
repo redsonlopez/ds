6edc679d07227356684e152adabbe97dcfed4497
redsonlopez <redsonlopez@gmail.com>
2026-04-02 13:33:56 -0300

    add sidebar

--- components/base.js
+++ components/base.js
@@ -1,4 +1,3 @@
-// components/base.js — inclua antes de tudo
 const isProd = location.hostname.includes('github.io');
 const base = document.createElement('base');
 base.href = isProd ? '/ds/' : '/';
--- components/site-sidebar.js
+++ components/site-sidebar.js
@@ -1,11 +1,9 @@
 class SiteSidebar extends HTMLElement {
   connectedCallback() {
-    //const base = document.querySelector('base')?.href || '/';
 
     this.innerHTML = `
       <nav>
         <h2>Fundamentos</h2>
-        <!-- <a href="${base}ml/01_fundamentals/01_empirical_risk.html">Risco Empírico</a> -->
         <a href="ml/01_fundamentals/01_empirical_risk.html">Risco Empírico</a>
         <a href="ml/01_fundamentals/05_loss_functions.html">Funções de Perda</a>
         <a href="ml/01_fundamentals/02_bias_variance.html">Bias vs Variance</a>
